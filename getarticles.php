3c403703ad12b4ef2e8b3953833ae6ee693facb8
Sth0nian <kingtoi@gmail.com>
2014-10-28 16:22:53 +0100

    c

--- getarticles.php
+++ getarticles.php
@@ -1,9 +1,9 @@
  <?php
- $dir = "./articles/";
+ $dir = "articles";
 
 // Open a known directory, and proceed to read its contents
 foreach(glob($dir) as $file) 
 {
-	echo $file;
+	echo file_get_contents($file, FILE_USE_INCLUDE_PATH). "<br />";
 }
 ?>
