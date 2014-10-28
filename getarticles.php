1f5da20d4a52ed5a5339e8ecd65e9f2198a73256
Sth0nian <kingtoi@gmail.com>
2014-10-28 17:04:30 +0100

    c

--- getarticles.php
+++ getarticles.php
@@ -1,9 +1,18 @@
  <?php
- $dir = "articles";
+ $dir = "articles/";
 
 // Open a known directory, and proceed to read its contents
+ /*
 foreach(glob($dir) as $file) 
-{
-	echo file_get_contents($file, FILE_USE_INCLUDE_PATH). "<br />";
+{	
+	$thing = file_get_contents($file);
+	echo $thing . "<br />";
+}*/
+$articles="";
+$files2 = scandir($dir, 1);
+$limit = sizeof($files2)-2;
+for($x=0; $x<$limit; $x++) {
+	$articles = $articles . file_get_contents($dir.$files2[$x]) . "<br/>\r\n\r\n\r\n";
 }
+echo $articles;
 ?>
